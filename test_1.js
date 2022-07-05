66e5aae14457fca10bc36af5542095dec45178b5
praba kalaivani <108567902+prabajerry@users.noreply.github.com>
2022-07-05 12:23:01 +0530

    demo

--- test_1.js
+++ test_1.js
@@ -1,1 +1,19 @@
 console.log("hello");
+
+
+
+// if you creating new project in Git hub
+//   1. site -> new repo 
+//   2. Repo name
+//   3. add readme.md file // optional
+//   4. reate new repo button
+
+
+// on our system
+//  1. git repo link
+//  2. vs code 
+//  3. cmd -> git clone  <link>
+//  4. code changes 
+//  5. cmd -> git add .
+//  6. cmd -> git commit -m "your message"
+//  7. cmd -> git push
