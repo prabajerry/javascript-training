ae71dc5e0e87c16a1e7ca6af2566750478b6b4e5
mouni186 <mounikas1861999@gmail.com>
2022-07-08 12:46:52 +0530

    test : sachin abs

--- findlargeNUmber.js
+++ findlargeNUmber.js
@@ -1,21 +1,21 @@
-const findLargeInThree = function (n1,n2,n3) { //1
+const findLargeInThree = function (n1, n2, n3) { //1
 
-    let large_val =0
-    if ( n1> n2)  {//2
+    let large_val = 0
+    if (n1 > n2) {//2
         large_val = n1
-        
+
     } else { //2 //3
-         large_val = n2
-        
+        large_val = n2
+
     }//3
-    if(n3 > large_val){ //4
+    if (n3 > large_val) { //4
         large_val = n3
     }//4
     return large_val
-    
-}   //1
-console.log(findLargeInThree(100,500,70));
 
+}   //1
+console.log(findLargeInThree(100, 500, 70));
+// from sachin abs
 // scope  ==> block
 
 //  1. correct function to calll
