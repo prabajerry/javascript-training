8704a78ee01d07821762255c38dbb8652f47afba
praba kalaivani <108567902+prabajerry@users.noreply.github.com>
2022-07-11 13:07:32 +0530

    math function

---
+++ math.js
@@ -0,0 +1,10 @@
+var l =10
+var b =7
+var area_rectangle = l*b
+console.log(Math.round(area_rectangle));
+console.log(Math.atan(90));
+console.log(Math.sqrt(b));
+console.log(Math.PI);
+console.log(Math.cbrt(10));
+console.log(Math.cos(180));
+
