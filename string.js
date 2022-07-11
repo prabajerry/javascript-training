1fa51053cea1d1d917c533165c4b048f5014be15
praba kalaivani <108567902+prabajerry@users.noreply.github.com>
2022-07-11 12:26:55 +0530

    feat:string ,templating

---
+++ string.js
@@ -0,0 +1,25 @@
+const s ="praba"
+const s1 = "kalaivani"
+console.log(s.length);
+console.log(s.charAt(4));
+console.log(s.concat(s1));
+console.log(s.toUpperCase(s1));
+console.log(s.toLowerCase(s));
+console.log(s.slice(0,2));
+console.log(s1.replace("kalaivani","computer"));
+
+// math formula ==>(a+b)2=a2+2ab+b2
+
+var a =3
+var b =5
+var lhs = (a+b)*(a+b)
+var rhs = a*a+2*(a*b)+b*b // fix
+console.log(lhs);
+console.log(rhs);
+
+// math functions
+// 
+let number = 90
+console.log(Math.sqrt(number));
+let vak = 7.8;
+console.log(Math.round(vak))
---
+++ template.js
@@ -0,0 +1,7 @@
+const name2 = "praba" 
+const name = "welcome to javascript"
+console.log(name);
+const message= ` hello ${name2} welcome to our 
+first java script program `
+console.log(message);
+//Template = ``
