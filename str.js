3ef6381dea98b3c67ae4ebdf7a7f909da4a0d4af
praba kalaivani <108567902+prabajerry@users.noreply.github.com>
2022-07-14 13:58:26 +0530

    print a value in array

--- str.js
+++ str.js
@@ -1,5 +1,17 @@
 const arr =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-console.log(arr[0]);
+/*
+step1:insert a-z in arr
+step2:create a random number range 0-25
+step3:round value
+step4:use rounded value us array index
+*/ 
+var randomnumber = Math.floor(Math.random() * 25);
+var flag =Math.floor(Math.random()*2)
+console.log(flag);
+if(flag==1){
+    console.log(arr[randomnumber].toUpperCase());
 
-
- 
+}
+else{
+    console.log(arr[randomnumber]);
+}
--- try.js
+++ try.js
@@ -1,7 +1,7 @@
 var num = 0
 var num1 = 3
 try{
-var quoient = num/Number
+var quoient = num/num1
 console.log(quoient);
 }catch(error){
     console.log("error:"+error.sorryerror); 
