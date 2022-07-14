1b959a24b2935c81809fbca78e9a95d019d31054
praba kalaivani <108567902+prabajerry@users.noreply.github.com>
2022-07-14 11:22:58 +0530

    area of triangle

--- area.js
+++ area.js
@@ -1,9 +1,7 @@
-var a =5
-var b=6
-var c=7
-var s=(a+b+c)/2
-console.log(Math.round(s));
-var x = s-a
-var y = s-b
-var z = s-c
-console.log(Math.sqrt(s*x*y*z));
+var base =5
+var height=6
+var height1=7
+var slope=(base+height+height1)/2
+console.log(Math.round(slope));
+var area = (slope*(slope-base)*(slope-height)*(slope-height1));
+console.log(Math.sqrt(area));
---
+++ try.js
@@ -0,0 +1,10 @@
+var num = 0
+var num1 = 3
+try{
+var quoient = num/Number
+console.log(quoient);
+}catch(error){
+    console.log(());
+
+}
+
