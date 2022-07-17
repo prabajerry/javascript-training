511e988ece81d21ee3712c628b8472d4704b13ec
praba kalaivani <108567902+prabajerry@users.noreply.github.com>
2022-07-17 21:07:57 +0530

    datefunction

---
+++ date.js
@@ -0,0 +1,7 @@
+const demo = new Date()
+console.log(demo.getDate());
+console.log(demo.getMonth());
+console.log(demo.getTime())
+console.log(demo.getDay());
+console.log(demo.getFullYear());
+console.log(demo.getHours());
